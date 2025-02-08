ede117ee58660d8b11e2284840e6d2a9c8390930
rrm2025 <auge00zonasul@gmail.com>
2025-02-07 21:44:53 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-let currentBanner = 0;
-const banners = ["banner1.jpg", "banner2.jpg", "banner3.jpg", "banner4.jpg", "banner5.jpg", "banner6.jpg"];
 
-function changeBanner() {
-    currentBanner = (currentBanner + 1) % banners.length;
-    document.getElementById('bannerImage').src = banners[currentBanner];
-}
-
-setInterval(changeBanner, 5000); // Troca o banner a cada 5 segundos
